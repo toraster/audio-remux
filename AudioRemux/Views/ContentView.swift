6b7b0cbeb230a7b1ed65bdc62327b12c30962a07
toraster <torastermix@gmail.com>
2026-01-19 17:21:44 +0900

    高さ調整

--- AudioRemux/Views/ContentView.swift
+++ AudioRemux/Views/ContentView.swift
@@ -41,7 +41,7 @@ struct ContentView: View {
                 .padding(.horizontal, 16)
                 .padding(.vertical, 8)
         }
-        .frame(minWidth: 950, minHeight: 560)
+        .frame(minWidth: 950, minHeight: 650)
         .background(Color(NSColor.windowBackgroundColor))
         // 自動波形表示: 両方のファイルがセットされたら自動的に波形を生成
         .onChange(of: viewModel.project.isReady) { isReady in
